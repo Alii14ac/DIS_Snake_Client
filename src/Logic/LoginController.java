0e77851a1248acc3772fe9217f41cf5a1152684d
alexanderlindkjaer <alindkjaer@gmail.com>
2015-11-17 00:21:37 +0100

    Starting on javafx..still learning

---
+++ src/Logic/LoginController.java
@@ -0,0 +1,29 @@
+package Logic;
+
+/**
+ * Created by alexanderlindkjaer on 17/11/2015.
+ */
+
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class LoginController {
+
+
+    public Button bacon;
+    public Label ham;
+
+
+    public void action(){
+
+        ham.setText("I also love me som Hammm");
+        bacon.setText("Stop touching mee!");
+
+    }
+
+
+
+
+}
